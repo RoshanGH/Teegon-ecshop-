21d8df7da896c18937a0ed104ed727f3d2215501
Roshan <275509523@qq.com>
2016-12-01 20:00:50 +0800

    修复ecshop充值支付成功后系统未更新

--- includes/modules/payment/tiangong.php
+++ includes/modules/payment/tiangong.php
@@ -99,18 +99,23 @@ class tiangong
         $param['order_no'] = $order['order_sn']; //订单号
         $param['channel'] = 'alipay';
         $param['return_url'] = return_url(basename(__FILE__, '.php'));
-       // $param['return_url'] = 'http://www.qq.com';
+        // $param['return_url'] = 'http://www.qq.com';
         $param['amount'] = $order['order_amount'];
         $param['subject'] =iconv('GBK','UTF-8',$name);
-        $param['metadata'] = "tiangong";
+        if(!$order['goods_amount'])
+        {
+            $param['metadata'] = "chongzhi";
+        }else{
+            $param['metadata'] = "";
+        }
         //$param['notify_url'] = 'http://www.baidu.com';//支付成功后天工收银网关通知
         $param['notify_url'] = return_url(basename(__FILE__, '.php'));
         $param['client_ip'] = $_SERVER["REMOTE_ADDR"];
         $param['client_id'] = $payment['tiangong_client_id'];
         $param['sign'] = $this->sign($param,$payment);
 
-//        error_log(print_r($order,1)."\n~~~~",3,"/Users/roshan/www/ecshop/admin/ecshop.log");
-//        error_log(print_r($param,1)."\n~~~~",3,"/Users/roshan/www/ecshop/admin/ecshop.log");
+        //        error_log(print_r($order,1)."\n~~~~",3,"/Users/roshan/www/ecshop/admin/ecshop.log");
+        //        error_log(print_r($param,1)."\n~~~~",3,"/Users/roshan/www/ecshop/admin/ecshop.log");
         //error_log(print_r($payment,1)."\n~~~~",3,"/Users/roshan/www/ecshop/admin/ecshop.log");
 
         $def_url  = '<div style="text-align:center"><form name="tiangong" accept-charset="UTF-8" style="text-align:center;" method="post" action="https://api.teegon.com/charge/pay" target="_blank">';
@@ -147,14 +152,19 @@ class tiangong
         //$_GET['data']=json_decode($_GET['data'],true);
 
         //验证签名
-       // echo "<pre/>";
+        // echo "<pre/>";
         unset($_GET['code']);
         $resign = $this->sign($_GET,$payment);
         //print_r($_GET);
         //print_r($resign);exit;
 
         //获取paid
-        $pay_id = get_order_id_by_sn($_GET['order_no']);
+        if($_GET['metadata'] == 'chongzhi')
+        {
+            $pay_id = get_order_id_by_sn($_GET['order_no'],true);
+        }else{
+            $pay_id = get_order_id_by_sn($_GET['order_no']);
+        }
 
         /* 检查支付的金额是否相符 */
         if (!check_money($pay_id, $_GET['amount']))
@@ -167,84 +177,13 @@ class tiangong
         {
             /* 改变订单状态 */
             order_paid($pay_id, 2);
-            if(!empty($_POST))
-            {
             return true;
         }else{
             return false;
         }
-
-
-
     }
-//    function respond()
-//    {
-//        if (!empty($_POST))
-//        {
-//            foreach($_POST as $key => $data)
-//            {
-//                $_GET[$key] = $data;
-//            }
-//        }
-//        $payment  = get_payment($_GET['code']);
-//        $seller_email = rawurldecode($_GET['seller_email']);
-//        $order_sn = str_replace($_GET['subject'], '', $_GET['out_trade_no']);
-//        $order_sn = trim($order_sn);
-//
-//        /* 检查数字签名是否正确 */
-//        ksort($_GET);
-//        reset($_GET);
-//
-//        $sign = '';
-//        foreach ($_GET AS $key=>$val)
-//        {
-//            if ($key != 'sign' && $key != 'sign_type' && $key != 'code')
-//            {
-//                $sign .= "$key=$val&";
-//            }
-//        }
-//
-//        $sign = substr($sign, 0, -1) . $payment['alipay_key'];
-//        //$sign = substr($sign, 0, -1) . ALIPAY_AUTH;
-//        if (md5($sign) != $_GET['sign'])
-//        {
-//            return false;
-//        }
-//
-//        /* 检查支付的金额是否相符 */
-//        if (!check_money($order_sn, $_GET['total_fee']))
-//        {
-//            return false;
-//        }
-//
-//        if ($_GET['trade_status'] == 'WAIT_SELLER_SEND_GOODS')
-//        {
-//            /* 改变订单状态 */
-//            order_paid($order_sn, 2);
-//
-//            return true;
-//        }
-//        elseif ($_GET['trade_status'] == 'TRADE_FINISHED')
-//        {
-//            /* 改变订单状态 */
-//            order_paid($order_sn);
-//
-//            return true;
-//        }
-//        elseif ($_GET['trade_status'] == 'TRADE_SUCCESS')
-//        {
-//            /* 改变订单状态 */
-//            order_paid($order_sn, 2);
-//
-//            return true;
-//        }
-//        else
-//        {
-//            return false;
-//        }
-//    }
-//}
-//tiangong 加密算法
+
+    //tiangong 加密算法
     public function sign($para_temp,$payment){
         //除去待签名参数数组中的空值和签名参数
         $para_filter = $this->para_filter($para_temp);
